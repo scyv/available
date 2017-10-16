db56e11e241960be572ee25c516e3d21757f3e1b
Yves Schubert <yves.schubert@posteo.de>
2017-10-16 23:42:38 +0200

    Add checkboxes for availabilities

--- client/js/availabilities.js
+++ client/js/availabilities.js
@@ -26,17 +26,53 @@ Template.availabilities.helpers({
         return !availabilitiesHandle.ready();
     },
     availabilities() {
-        return Availabilities.find({sprintId: Session.get(SessionProps.SELECTED_SPRINT)});
+        const availabilities = Availabilities.find({sprintId: Session.get(SessionProps.SELECTED_SPRINT)});
+        const sprintVelocity = getSprintVelocity();
+        const selectedProject = Projects.findOne(Session.get(SessionProps.SELECTED_PROJECT));
+        let sumAvailability = 0;
+        let sumPossibleStoryPoints = 0;
+        availabilities.forEach((av)=> {
+            const availabilityInDays = (av.availability / selectedProject.hoursPerDay);
+            const possibleSP = (availabilityInDays * sprintVelocity);
+            const avWindow = Session.get(SessionProps.AVAILABILITY_WINDOW);
+            Session.set(SessionProps.AVAILABILITY_POSSIBLE_SP + av._id, possibleSP);
+
+            if (avWindow.length === 0 || avWindow.includes(av._id)) {
+                sumAvailability += availabilityInDays;
+                sumPossibleStoryPoints += possibleSP;
+            }
+        });
+        Session.set(SessionProps.AVAILABILITY_SUM,
+            (selectedProject.hoursPerDay * sumAvailability).toFixed(0)
+            + " / " + sumAvailability.toFixed(1));
+        Session.set(SessionProps.AVAILABILITY_SP_SUM, sumPossibleStoryPoints.toFixed(2));
+
+        return availabilities;
+    },
+    availability() {
+        const selectedProject = Projects.findOne(Session.get(SessionProps.SELECTED_PROJECT));
+        return this.availability + " / " + (this.availability / selectedProject.hoursPerDay).toFixed(1);
     },
     sprintVelocity() {
         return getSprintVelocity();
     },
     possibleSP() {
-        const sprintVelocity = getSprintVelocity();
-        const selectedProject = Projects.findOne(Session.get(SessionProps.SELECTED_PROJECT));
-
-        return ((this.availability / selectedProject.hoursPerDay) * sprintVelocity).toFixed(2);
+        const possibleSP = Session.get(SessionProps.AVAILABILITY_POSSIBLE_SP + this._id)
+        if (!possibleSP) {
+            return 0;
+        }
+        return possibleSP.toFixed(2);
+    },
+    availabilitySum() {
+        return Session.get(SessionProps.AVAILABILITY_SUM);
+    },
+    storyPointSum() {
+        return Session.get(SessionProps.AVAILABILITY_SP_SUM);
     }
+});
+
+Template.availabilities.onRendered(()=> {
+    Session.set(SessionProps.AVAILABILITY_WINDOW, []);
 });
 
 Template.availabilities.events({
@@ -47,5 +83,26 @@ Template.availabilities.events({
     'click .btnEditAvailability'(event, instance) {
         Session.set(SessionProps.SELECTED_AVAILABILITY, this._id);
         $('#dlgEditAvailability').modal('show');
+    },
+    'change .checkForSum'(evt) {
+        let avWindow = Session.get(SessionProps.AVAILABILITY_WINDOW);
+        if (evt.target.checked) {
+            avWindow.push(this._id);
+            $(evt.target).parentsUntil("tbody", "tr").addClass("for-calculation");
+        } else {
+            avWindow = _.without(avWindow, this._id);
+            $(evt.target).parentsUntil("tbody", "tr").removeClass("for-calculation");
+        }
+
+        Session.set(SessionProps.AVAILABILITY_WINDOW, avWindow);
+    },
+    'click .btnSelectAll'() {
+        $(".availabilityRow .checkForSum").not(":checked").click();
+    },
+    'click .btnSelectInvert'() {
+        $(".availabilityRow .checkForSum").click();
+    },
+    'click .btnSelectNone'() {
+        $(".availabilityRow .checkForSum:checked").click();
     }
 });
--- client/js/sessionProperties.js
+++ client/js/sessionProperties.js
@@ -1,5 +1,10 @@
 export const SessionProps = {
     SELECTED_PROJECT: "selectedProject",
     SELECTED_SPRINT: "selectedSprint",
-    SELECTED_AVAILABILITY: "selectedAvailability"
+    SELECTED_AVAILABILITY: "selectedAvailability",
+
+    AVAILABILITY_WINDOW: "availabilityWindow",
+    AVAILABILITY_POSSIBLE_SP: "availabilityPossibleSP-",
+    AVAILABILITY_SUM: "availabilitySum",
+    AVAILABILITY_SP_SUM: "availabilitySpSum",
 };
